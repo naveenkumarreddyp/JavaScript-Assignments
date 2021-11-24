0a24722fb3f842e1cf55115f3d9ea8b83d209e59
Naveen <naveenreddyp9580@gmail.com>
2021-11-24 23:03:55 +0530

    commit-1

---
+++ assignments/favourite_movie/favourite_movie.js
@@ -0,0 +1,16 @@
+const movies = [];
+
+function favouriteMovie(operation, movie) {
+    // Write your code here
+    if (operation =="add"){
+        movies.push(movie)
+        return(movies)
+    } else{
+        movies.pop()
+        return(movies)
+    }
+
+    
+}
+
+module.exports = favouriteMovie;

a142d7f77f58eef53896b06b95c28986b6bf0db7
Naveen <naveenreddyp9580@gmail.com>
2021-11-28 15:41:49 +0530

    RunCallback

--- assignments/run_callback/run_callback.js
+++ assignments/run_callback/run_callback.js
@@ -1,6 +1,6 @@
 function RunCallback(a, b, cb) {
     // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
-    let Sum = a + b ;
+    var Sum = a + b ;
     return(cb(Sum)) ;
 }
 
